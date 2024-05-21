36f9c48cc758ea40550c1f34f66cd4d024f46418
Dionte Driskell <driskell06@gmail.com>
2024-05-21 16:33:29 -0400

    finally working

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ async function fetchCatImages() {
     const apiKey = 'live_dl5xdMlc2KCjXSq2qFVzHPnob4Qv89NuUf2mCvixGtDYBACe6JiaJefx0HsUAuvM'; 
     const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=${apiKey}`);
     const catsData = await response.json();
-    const catGallery = document.getElementById('cat-gallery');
+    const catDropdown = document.getElementById('catDropdown');
 
     catsData.forEach(cat => {
         const img = document.createElement('img');
         img.src = cat.url;
         img.alt = 'Cat';
         // catGallery.body.appendChild(img);
-        catGallery.appendChild(img);
+        catDropdown.appendChild(img);
     });
 }
+function toggleDropdown() {
+    const dropdownContent = document.getElementById('catDropdown');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+}
+
 
 
 fetchCatImages();
