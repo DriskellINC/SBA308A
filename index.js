da2d85abf6e148570620dc1f9e95d051703d1452
Dionte Driskell <driskell06@gmail.com>
2024-05-21 09:05:56 -0400

    bugs fixed

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-curl --location 'https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_dl5xdMlc2KCjXSq2qFVzHPnob4Qv89NuUf2mCvixGtDYBACe6JiaJefx0HsUAuvM%0A%0A'
+// curl --location 'https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_dl5xdMlc2KCjXSq2qFVzHPnob4Qv89NuUf2mCvixGtDYBACe6JiaJefx0HsUAuvM%0A%0A'
 
-async function apiCat() {
-    const response = await fetch("curl --location 'https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_dl5xdMlc2KCjXSq2qFVzHPnob4Qv89NuUf2mCvixGtDYBACe6JiaJefx0HsUAuvM%0A%0A'
-);
-    const movies = await response.json();
-    console.log(apiCat);
-  }
-
-  sync function fetchCatImages() {
-    const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+async function fetchCatImages() {
+    const apiKey = 'live_dl5xdMlc2KCjXSq2qFVzHPnob4Qv89NuUf2mCvixGtDYBACe6JiaJefx0HsUAuvM'; 
     const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=${apiKey}`);
     const catsData = await response.json();
     const catGallery = document.getElementById('cat-gallery');
@@ -17,9 +10,9 @@ async function apiCat() {
         const img = document.createElement('img');
         img.src = cat.url;
         img.alt = 'Cat';
+        // catGallery.body.appendChild(img);
         catGallery.appendChild(img);
     });
 }
 
 fetchCatImages();
-
